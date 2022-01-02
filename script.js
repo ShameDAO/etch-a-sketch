d8042f4cb22b9aeef99ed3305b1a76cb655fb21c
shame <shame.bdao@gmail.com>
2022-01-02 15:31:43 +1100

    Added 16x16 grid

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-const container = document.querySelector(".container");
+const length = 16;
+const height = 16;
+const numSquares = length * height;
 
+const container = document.querySelector(".container");
+let squareDiv;
+
+for (let i = 0; i < numSquares; i++) {
+    squareDiv = document.createElement("div");
+    squareDiv.classList.add("square");
+    
+    // if it is the last square in the row, move to next row with float/clear
+    if ((i % length === 0) && i !== 0) {
+        squareDiv.style.clear = "left";
+    }
+
+    container.appendChild(squareDiv);
+}
